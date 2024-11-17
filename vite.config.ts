3670df0b4002f4af9a8a953950c32f207f5e2b96
GrimReaper <parasraina.33@gmail.com>
2024-11-18 01:35:53 +0530

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react(),eslint()],
+  base:'/vite-project/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
